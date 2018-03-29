42d101b8371c4ff086817900fef5ab62f1ff5e0e
祐任 林 <ikaros45625@gmail.com>
2018-03-30 03:06:41 +0800

    test0329

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument xdoc = new XmlDocument();
+            xdoc.Load("CWBEQ.xml");
+            
+        }
+    }
+}
